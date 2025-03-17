948287d6a0db7b244ff34ec0978b28b619926ef5
Zain Zia <zainzia1992@gmail.com>
2025-03-17 11:49:51 +0500

    Updated port configuration

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def upload_image():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
